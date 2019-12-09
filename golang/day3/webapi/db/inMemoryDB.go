c0dbe318488ab9dafa7c20b3f4903b867613b041
Jorge Castro <jorge.castro.au@gmail.com>
2019-12-09 00:30:59 -0600

    dockerfile debug and release stage added

--- golang/day3/webapi/db/inMemoryDB.go
+++ golang/day3/webapi/db/inMemoryDB.go
@@ -2,17 +2,24 @@ package db
 
 import "github.com/kodran/workshop/day3/webapi/model"
 
+// Client table model
 var Client = []model.Client{
 	model.Client{
-		ID:   1,
-		Name: "1",
+		ID:       1,
+		Name:     "Jorge",
+		LastName: "Castro",
+		Age:      30,
 	},
 	model.Client{
-		ID:   2,
-		Name: "2",
+		ID:       2,
+		Name:     "Roberto",
+		LastName: "Garcia",
+		Age:      30,
 	},
 	model.Client{
-		ID:   3,
-		Name: "3",
+		ID:       3,
+		Name:     "Max",
+		LastName: "Deboli",
+		Age:      36,
 	},
 }
--- golang/day3/webapi/go.mod
+++ golang/day3/webapi/go.mod
@@ -5,4 +5,5 @@ go 1.12
 require (
 	github.com/go-delve/delve v1.3.2 // indirect
 	github.com/gorilla/mux v1.7.3
+	github.com/stretchr/testify v1.3.0
 )
